3f5c80e66032fd283e9fe9aab89cb094148c5f51
Valium007 <49368101+Valium007@users.noreply.github.com>
2024-02-13 05:11:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ rm *.xz
 CC=$WORK_DIR/gcc/bin/arm-linux-gnueabihf-gcc
 CXX=$WORK_DIR/gcc/bin/arm-linux-gnueabihf-g++
 
-git clone https://github.com/llvm/llvm-project --depth 1 -b release/15.x
+git clone https://github.com/llvm/llvm-project --depth 1 -b release/17.x
 mkdir -p llvm-project/bhost
 mkdir -p llvm-project/build
 
@@ -20,7 +20,7 @@ cmake -G Ninja ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="ARM;X
 ninja clang-tblgen llvm-tblgen
 
 cd $WORK_DIR/llvm-project/build
-cmake -G Ninja ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="ARM;X86" -DLLVM_DEFAULT_TARGET_TRIPLE=arm-linux-gnueabihf -DLLVM_TARGET_ARCH=ARM -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DLLVM_ENABLE_PIC=ON -DLLVM_ENABLE_PROJECTS="lld;clang;compiler-rt" -DLLVM_TABLEGEN=$WORK_DIR/llvm-project/bhost/bin/llvm-tblgen -DCLANG_TABLEGEN=$WORK_DIR/llvm-project/bhost/bin/clang-tblgen -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_BUILD_LLVM_DYLIB=On -DLLVM_LINK_LLVM_DYLIB=On -DLLVM_INSTALL_TOOLCHAIN_ONLY=On -DCMAKE_INSTALL_PREFIX=$WORK_DIR/arm-install
+cmake -G Ninja ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="ARM;X86" -DLLVM_DEFAULT_TARGET_TRIPLE=arm-linux-gnueabihf -DLLVM_TARGET_ARCH=ARM -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DLLVM_ENABLE_PIC=ON -DLLVM_ENABLE_PROJECTS="lld;clang;compiler-rt" -DLLVM_TABLEGEN=$WORK_DIR/llvm-project/bhost/bin/llvm-tblgen -DCLANG_TABLEGEN=$WORK_DIR/llvm-project/bhost/bin/clang-tblgen -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_BUILD_LLVM_DYLIB=On -DLLVM_LINK_LLVM_DYLIB=On -DLLVM_INSTALL_TOOLCHAIN_ONLY=On -DLLVM_INCLUDE_TESTS=OFF -DCLANG_INCLUDE_TESTS=OFF -DCLANG_TOOL_C_INDEX_TEST_BUILD=OFF -DLLVM_ENABLE_LIBEDIT=OFF -DCMAKE_INSTALL_PREFIX=$WORK_DIR/arm-install
 ninja -j4 && ninja install
 
 ls $WORK_DIR/arm-install
