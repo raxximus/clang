c836be9a445c27cbad2c88dfaa083aeef17d4ba3
Valium007 <49368101+Valium007@users.noreply.github.com>
2024-02-13 21:52:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 WORK_DIR=$GITHUB_WORKSPACE
+VERSION=17
 
 cd $WORK_DIR
 wget https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/arm-linux-gnueabihf/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz
@@ -11,7 +12,7 @@ rm *.xz
 CC=$WORK_DIR/gcc/bin/arm-linux-gnueabihf-gcc
 CXX=$WORK_DIR/gcc/bin/arm-linux-gnueabihf-g++
 
-git clone https://github.com/llvm/llvm-project --depth 1 -b release/17.x
+git clone https://github.com/llvm/llvm-project --depth 1 -b release/$VERSION.x
 mkdir -p llvm-project/bhost
 mkdir -p llvm-project/build
 
@@ -23,5 +24,5 @@ cd $WORK_DIR/llvm-project/build
 cmake -G Ninja ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="ARM;X86" -DLLVM_DEFAULT_TARGET_TRIPLE=arm-linux-gnueabihf -DLLVM_TARGET_ARCH=ARM -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DLLVM_ENABLE_PIC=ON -DLLVM_ENABLE_PROJECTS="lld;clang;compiler-rt" -DLLVM_TABLEGEN=$WORK_DIR/llvm-project/bhost/bin/llvm-tblgen -DCLANG_TABLEGEN=$WORK_DIR/llvm-project/bhost/bin/clang-tblgen -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_BUILD_LLVM_DYLIB=On -DLLVM_LINK_LLVM_DYLIB=On -DLLVM_INSTALL_TOOLCHAIN_ONLY=On -DLLVM_INCLUDE_TESTS=OFF -DCLANG_INCLUDE_TESTS=OFF -DCLANG_TOOL_C_INDEX_TEST_BUILD=OFF -DLLVM_ENABLE_LIBEDIT=OFF -DCMAKE_INSTALL_PREFIX=$WORK_DIR/arm-install -DLLVM_INCLUDE_TESTS=OFF -DCLANG_INCLUDE_TESTS=OFF -DCLANG_TOOL_C_INDEX_TEST_BUILD=OFF -DLLVM_ENABLE_LIBEDIT=OFF -DCMAKE_SYSTEM_NAME="Linux" -DCMAKE_CROSSCOMPILING=True -DCLANG_DEFAULT_LINKER=lld
 ninja -j4 && ninja install
 
-tar --xz -cf $WORK_DIR/arm.tar $WORK_DIR/arm-install/
-
+tar --xz -cf $WORK_DIR/clang-arm.tar $WORK_DIR/arm-install/
+gh release create v$VERSION $WORK_DIR/clang-arm.tar -R valium007/clang -t "LLVM/Clang $VERSION" -n "LLVM/Clang $VERSION for arm linux"
