b4891eb9cf2b26a0c21d35b2d477d899f21602f8
raxximus <valium007@proton.me>
2024-07-02 23:06:54 +0530

    updated

--- build.sh
+++ build.sh
@@ -1,27 +1,56 @@
 #!/usr/bin/env bash
 WORK_DIR=$GITHUB_WORKSPACE
-VERSION=17
+VERSION=18
 
+cd /usr/x86_64-w64-mingw32/include
+curl -O https://gist.githubusercontent.com/NZSmartie/079d8f894ee94f3035306cb23d49addc/raw/550bebfd895fa5cf4d8bfdc360161d85c0deedf1/afunix.h
 cd $WORK_DIR
-wget https://snapshots.linaro.org/gnu-toolchain/12.3-2023.06-1/arm-linux-gnueabihf/gcc-linaro-12.3.1-2023.06-x86_64_arm-linux-gnueabihf.tar.xz
-tar -xf *.xz
-rm *.xz
-
-CC=$WORK_DIR/gcc-linaro-12.3.1-2023.06-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc
-CXX=$WORK_DIR/gcc-linaro-12.3.1-2023.06-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-g++
 
 git clone https://github.com/llvm/llvm-project --depth 1 -b release/$VERSION.x
 mkdir -p llvm-project/bhost
 mkdir -p llvm-project/build
 
 cd llvm-project/bhost
-cmake -G Ninja ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="ARM;X86" -DLLVM_ENABLE_PROJECTS="lld;clang;compiler-rt"
+cmake -G Ninja ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_ENABLE_PROJECTS="lld;clang;compiler-rt"
 ninja clang-tblgen llvm-tblgen
 
 cd $WORK_DIR/llvm-project/build
-cmake -G Ninja ../llvm -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="ARM;X86" -DLLVM_DEFAULT_TARGET_TRIPLE=arm-linux-gnueabihf -DLLVM_TARGET_ARCH=ARM -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DLLVM_ENABLE_PIC=ON -DLLVM_ENABLE_PROJECTS="lld;clang;compiler-rt" -DLLVM_TABLEGEN=$WORK_DIR/llvm-project/bhost/bin/llvm-tblgen -DCLANG_TABLEGEN=$WORK_DIR/llvm-project/bhost/bin/clang-tblgen -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_BUILD_LLVM_DYLIB=On -DLLVM_LINK_LLVM_DYLIB=On -DLLVM_INSTALL_TOOLCHAIN_ONLY=On -DLLVM_INCLUDE_TESTS=OFF -DCLANG_INCLUDE_TESTS=OFF -DCLANG_TOOL_C_INDEX_TEST_BUILD=OFF -DLLVM_ENABLE_LIBEDIT=OFF -DCMAKE_INSTALL_PREFIX=$WORK_DIR/clang-arm -DLLVM_INCLUDE_TESTS=OFF -DCLANG_INCLUDE_TESTS=OFF -DCLANG_TOOL_C_INDEX_TEST_BUILD=OFF -DLLVM_ENABLE_LIBEDIT=OFF -DCMAKE_SYSTEM_NAME="Linux" -DCMAKE_CROSSCOMPILING=True -DCLANG_DEFAULT_LINKER=lld
+cmake -G Ninja ../llvm \
+-DCMAKE_BUILD_TYPE=Release \
+-DLLVM_TARGETS_TO_BUILD="ARM;AArch64;X86" \
+-DLLVM_DEFAULT_TARGET_TRIPLE=arm-linux-musleabihf \
+-DLLVM_TARGET_ARCH=ARM \
+-DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc-posix \
+-DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++-posix \
+-DLLVM_ENABLE_PIC=ON \
+-DLLVM_ENABLE_PROJECTS="lld;clang" \
+-DLLVM_TABLEGEN=$WORK_DIR/llvm-project/bhost/bin/llvm-tblgen \
+-DCLANG_TABLEGEN=$WORK_DIR/llvm-project/bhost/bin/clang-tblgen \
+-DLLVM_PARALLEL_LINK_JOBS=1 \
+-DLLVM_BUILD_LLVM_DYLIB=ON \
+-DLLVM_LINK_LLVM_DYLIB=ON \
+-DLLVM_INSTALL_TOOLCHAIN_ONLY=ON \
+-DLLVM_INCLUDE_TESTS=OFF \
+-DCLANG_INCLUDE_TESTS=OFF \
+-DCLANG_TOOL_C_INDEX_TEST_BUILD=OFF \
+-DLLVM_ENABLE_LIBEDIT=OFF \
+-DCMAKE_INSTALL_PREFIX=$WORK_DIR/clang \
+-DCMAKE_SYSTEM_NAME="Windows" \
+-DCMAKE_CROSSCOMPILING=True \
+-DCLANG_DEFAULT_LINKER=lld \
+-DLLVM_BUILD_TOOLS=NO \
+-DDEFAULT_SYSROOT="../sysroot"
+
 ninja -j4 && ninja install
 
+DEST_DIR=$WORK_DIR/clang/bin
+FILES=(libatomic-1.dll libssp-0.dll libgcc_s_seh-1.dll>
+# Search for the files in all directories on the system
+for file in "${FILES[@]}"; do
+find /usr -type f -name "$file" -exec cp {} "$DEST_DIR" \; 2>&1 > /dev/null
+done
+
+
 cd $WORK_DIR
-tar --xz -cf clang-arm.tar clang-arm/
-gh release create v$VERSION clang-arm.tar -R valium007/clang -t "LLVM/Clang $VERSION" -n "LLVM/Clang $VERSION for arm linux"
+zip -r clang.zip
+gh release create $VERSION clang.zip -R raxximus/clang -t "LLVM/Clang $VERSION" -n "LLVM/Clang $VERSION" -p
